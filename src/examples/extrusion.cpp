68df90db2c9b324806bff9d39249ead583f41b16
Alex Steiger <alexander.steiger@uni-rostock.de>
2019-07-31 14:16:45 +0200

    14

--- src/examples/extrusion.cpp
+++ src/examples/extrusion.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <QString>
 //PCL:
-#include <pcl/io/pcd_io.h>  //for PointCloud
+#include <pcl/io/pcd_io.h>
 #include <pcl/common/common.h> //for getMinMax3D
 #include <pcl/ModelCoefficients.h>  //for planar coefficients
 #include <pcl/filters/project_inliers.h>
@@ -18,39 +18,9 @@
 #include <gp_Vec.hxx>
 #include <BRepBuilderAPI_MakeFace.hxx>
 #include <BRepBuilderAPI_MakePolygon.hxx>
-#include <BRepPrimAPI_MakeHalfSpace.hxx>
-#include <BRepAlgoAPI_Cut.hxx>
 #include <BRepPrimAPI_MakePrism.hxx>
 #include <TopoDS.hxx>
-#include <TopoDS_Face.hxx>
-#include <TopoDS_Shape.hxx>
-#include <TopoDS_Solid.hxx>
 #include "STEPControl_Writer.hxx"
-#include "STEPControl_Reader.hxx"
-#include "StlAPI_Reader.hxx"
-#include <TopExp_Explorer.hxx>
-#include <Standard_PrimitiveTypes.hxx>
-#include <BRepAdaptor_Surface.hxx>
-
-//IFC:
-#ifdef USE_IFC4
-#include "../ifcparse/Ifc4.h"
-#else
-#include "../ifcparse/Ifc2x3.h"
-#endif
-
-#include "../ifcparse/IfcBaseClass.h"
-#include "../ifcparse/IfcHierarchyHelper.h"
-#include "../ifcgeom/IfcGeom.h"
-
-#if USE_VLD
-#include <vld.h>
-#endif
-
-// Some convenience typedefs and definitions. 
-typedef std::string S;
-typedef IfcParse::IfcGlobalId guid;
-boost::none_t const null = boost::none;
 
 int main(int argc, char *argv[]) {
 
@@ -91,12 +61,12 @@ int main(int argc, char *argv[]) {
   // Statistical outlier removal 1
   std::cout << "Starting Statistical outlier removal... ";
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered1 (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor2;
-  sor2.setInputCloud (cloud_input);
-  sor2.setMeanK (100);
-  sor2.setStddevMulThresh (5);
-  sor2.setNegative (false);
-  sor2.filter (*cloud_filtered1);
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor1;
+  sor1.setInputCloud (cloud_input);
+  sor1.setMeanK (100);
+  sor1.setStddevMulThresh (5);
+  sor1.setNegative (false);
+  sor1.filter (*cloud_filtered1);
   std::cout << cloud_input->points.size() - cloud_filtered1->points.size () << " outliers where removed. ";
   std::cout << cloud_filtered1->points.size() << " points left" << std::endl;
   
@@ -154,12 +124,12 @@ int main(int argc, char *argv[]) {
   
   // Statistical outlier removal 2
   std::cout << "Starting Statistical outlier removal of projected cloud... " ;
-  pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-  sor.setInputCloud (cloud_projected);
-  sor.setMeanK (40);
-  sor.setStddevMulThresh (2.5);
-  sor.setNegative (false);
-  sor.filter (*cloud_filtered);
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor2;
+  sor2.setInputCloud (cloud_projected);
+  sor2.setMeanK (40);
+  sor2.setStddevMulThresh (2.5);
+  sor2.setNegative (false);
+  sor2.filter (*cloud_filtered);
   std::cout << cloud_projected->points.size() - cloud_filtered->points.size ();
   std::cout << " outliers where removed. ";
   std::cout << cloud_filtered->points.size() << " points left" << std::endl;
@@ -196,34 +166,6 @@ int main(int argc, char *argv[]) {
   face = BRepLib_MakeFace (wire);
   solid = BRepPrimAPI_MakePrism(face,gp_Vec(0.,0.,minPt.z-maxPt.z));
 
-// 	std::cout << "starting reading the stl-file" << std::endl;
-//   StlAPI_Reader stlreader;
-// 	QString qfilename = "Verformung_cutout.stl";
-// 	TopoDS_Shape verformung_shape;
-// 	stlreader.Read(verformung_shape, (Standard_CString)qfilename.toLatin1().constData());
-// 	std::cout << "finish reading the stl-file" << std::endl;
-//  	
-// 	
-// 	std::cout << "building shell from faces... (one \"I\" for each face)" << std::endl;
-// 	BRep_Builder builder;
-// 	TopoDS_Shell verformung_shell;
-// 	builder.MakeShell(verformung_shell);
-// for( TopExp_Explorer ex(verformung_shape, TopAbs_FACE); ex.More(); ex.Next() )
-// {
-// 	TopoDS_Face currentFace = TopoDS::Face( ex.Current() );
-// 	BRepAdaptor_Surface brepAdaptorSurface( currentFace,Standard_True );
-// 	builder.Add(verformung_shell, currentFace);
-// 	//std::cout << "I";
-// }
-// 	
-// 	// Cut a solid with a face
-// 	// 1. Use BRepPrimAPI_MakeHalfSpace to create a half-space. 
-// 	// One side: 150.,0.,-5.
-// 	// Other side: 
-// 	TopoDS_Solid Halbraum = BRepPrimAPI_MakeHalfSpace(verformung_shell, gp_Pnt(150.,-100.,-5.)).Solid();
-// 	// 2. Use Boolean APIs do sub and intersection operations
-//  	TopoDS_Shape Remaining_Extrusion = BRepAlgoAPI_Cut(solid,Halbraum);
-
   //write Files
   pcl::PCDWriter writer;
   writer.write ("E0_Input_Cloud.pcd", *cloud_input, false);
@@ -235,66 +177,7 @@ int main(int argc, char *argv[]) {
   writer.write ("E6_Concave_Hull.pcd", *cloud_hull, false);
   STEPControl_Writer OCC_writer;
   OCC_writer.Transfer(solid,STEPControl_AsIs);
-	//OCC_writer.Transfer(verformung_shell,STEPControl_AsIs);
-	//OCC_writer.Write("E9_Remaining_Extrusion.stp");
   OCC_writer.Write("E7_Extrusion.stp");
-
-
-//////////////////////////////////
-//////////IFC STARTS HERE/////////
-//////////////////////////////////
-    
-  
-//   // The IfcHierarchyHelper is a subclass of the regular IfcFile that provides several
-//   // convenience functions for working with geometry in IFC files.
-//   IfcHierarchyHelper file;
-//   file.header().file_name().name("E8_Spundwand.ifc");
-// 
-//   // Start by adding a wall to the file, initially leaving most attributes blank.
-//   IfcSchema::IfcWallStandardCase* wall = new IfcSchema::IfcWallStandardCase(
-//     guid(), 		// GlobalId
-//     0, 					// OwnerHistory
-//     S("Spundwand"),     // Name
-//     S("Automatisch erstellt durch extrusion.cpp"), 	// Description
-//     null, 			// ObjectType
-//     0, 					// ObjectPlacement
-//     0, 					// Representation
-//     null				// Tag
-//   #ifdef USE_IFC4
-//     , IfcSchema::IfcWallTypeEnum::IfcWallType_STANDARD
-//   #endif
-//   );
-// 
-//   file.addBuildingProduct(wall);
-//   // By adding a wall, a hierarchy has been automatically created that consists of the following
-//   // structure: IfcProject > IfcSite > IfcBuilding > IfcBuildingStorey > IfcWall
-// 
-//   // An IfcOwnerHistory has been initialized as well, which should be assigned to the wall.
-//   wall->setOwnerHistory(file.getSingle<IfcSchema::IfcOwnerHistory>());
-// 
-//   // Since the solid consists only of planar faces and straight edges it can be serialized as an
-//   // IfcFacetedBRep. If it would not be a polyhedron, serialise() can only be successful when linked
-//   // to the IFC4 model and with `advanced` set to `true` which introduces IfcAdvancedFace. It would
-//   // return `0` otherwise.
-//   IfcSchema::IfcProductDefinitionShape* object_shape = IfcGeom::serialise(solid, false);
-//   file.addEntity(object_shape);
-//   IfcSchema::IfcRepresentation* rep = *object_shape->Representations()->begin();
-//   rep->setContextOfItems(file.getRepresentationContext("model"));
-//   wall->setRepresentation(object_shape);
-//   // A red colour is assigned to the wall.
-//   file.setSurfaceColour(object_shape, 0.55 , 0.25 , 0.05);
-//     
-//   // Obtain a reference to the placement of the IfcBuildingStorey in order to create a hierarchy of placements for the products
-//   IfcSchema::IfcObjectPlacement* storey_placement = file.getSingle<IfcSchema::IfcBuildingStorey>()->ObjectPlacement();
-// 
-//   // wall and is placed at the origin of the coordinate system.
-//   wall->setObjectPlacement(file.addLocalPlacement(storey_placement));
-// 
-// 
-// 
-//   std::ofstream f("E8_Spundwand.ifc");
-//   f << file;
-
 }
 
 
