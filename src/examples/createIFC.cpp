9ac133bc4884202fe9328c1f5f6ac78ab9d78e55
AlexSteiger <alexandersteiger@uni-rostock.de>
2019-08-14 10:09:08 +0200

    ifc ready

--- src/examples/createIFC.cpp
+++ src/examples/createIFC.cpp
@@ -13,9 +13,9 @@
 #endif
 
 // Some convenience typedefs and definitions. 
-typedef std::string S;
-typedef IfcParse::IfcGlobalId guid;
-boost::none_t const null = boost::none;
+//typedef std::string S;
+//typedef IfcParse::IfcGlobalId guid;
+//boost::none_t const null = boost::none;
 
 int main(int argc, char *argv[]) {
 
@@ -35,12 +35,12 @@ int main(int argc, char *argv[]) {
 	STEPControl_Reader stpreader;
 	stpreader.ReadFile(filename.c_str());
 	stpreader.TransferRoots();
-	TopoDS_Shape spwand_shape = stpreader.OneShape();
+	TopoDS_Shape spwand_shapeOCC = stpreader.OneShape();
   std::cout << "finish reading the stp-file" << std::endl;
 
-//////////////////////////////////
-//////////IFC STARTS HERE/////////
-//////////////////////////////////
+  //////////////////////////////////
+  //////////IFC STARTS HERE/////////
+  //////////////////////////////////
     
   // The IfcHierarchyHelper is a subclass of the regular IfcFile that provides several
   // convenience functions for working with geometry in IFC files.
@@ -49,43 +49,86 @@ int main(int argc, char *argv[]) {
 	// Writes the filename in the Header of the IFC file.
   file.header().file_name().name("Spundwand.ifc");
 
-  // Start by adding a wall to the file, initially leaving most attributes blank.
-  IfcSchema::IfcWall* wall = new IfcSchema::IfcWall(
-    guid(), 		// GlobalId
-    0, 					// OwnerHistory
-    S("Spundwand"),     // Name
-    S("Mönckebergkai (Ausschnitt)"), 	// Description
-    null, 			// ObjectType
-    0, 					// ObjectPlacement
-    0, 					// Representation
-    null				// Tag
-		, IfcSchema::IfcWallTypeEnum::IfcWallType_STANDARD
+  // Define a IfcOwnerHistory (optional) by adding a IfcPersonAndOrganization & IfcApplication
+  IfcSchema::IfcPersonAndOrganization* pando = new IfcSchema::IfcPersonAndOrganization
+  (
+    new IfcSchema::IfcPerson
+    (
+      boost::none,              // Identification
+      std::string("Steiger"),   // FamilyName
+      std::string("Alexander"), // GivenName
+      boost::none,              // MiddleNames
+      boost::none,              // PrefixTitles
+      boost::none,              // SuffixTitles
+      boost::none,              // Roles
+      boost::none               // Addresses
+    ),
+    new IfcSchema::IfcOrganization
+    (
+      boost::none,                                  // Identification
+      std::string("Uni Rostock | Fraunhofer IGD"),  // Name
+      boost::none,                                  // Description
+      boost::none,                                  // Roles
+      boost::none                                   // Addresses
+    ),
+    boost::none	// Roles
   );
+  IfcSchema::IfcApplication* appl = new IfcSchema::IfcApplication
+  (
+    pando->TheOrganization(),   // ApplicationDeveloper
+    "",                         // Version
+    "",                         // ApplicationFullName
+    ""                          // ApplicationIdentifier
+  );
+  file.addEntity(pando);
+  file.addEntity(appl);
+  IfcSchema::IfcOwnerHistory* history = new IfcSchema::IfcOwnerHistory
+  (
+    pando,                                            // OwningUser
+    appl,                                             // OwningApplication
+    boost::none,                                      // State
+    Ifc4::IfcChangeActionEnum::IfcChangeAction_ADDED, // ChangeAction
+    boost::none,                                      // LastModifiedDate
+    pando,                                            // LastModifyingUser
+    appl,                                             // LastModifyingApplication
+    std::time(0)                                      // CreationDate
+  );
+  file.addEntity(history);
 
-  file.addBuildingProduct(wall);
+  // Start by adding a wall to the file, initially leaving most attributes blank.
+  IfcSchema::IfcWall* spwand = new IfcSchema::IfcWall
+  (
+    IfcParse::IfcGlobalId(),                      // GlobalId
+    file.getSingle<IfcSchema::IfcOwnerHistory>(), // OwnerHistory
+    std::string("Spundwand"),                     // Name
+    std::string("Mönckebergkai (Ausschnitt)"),    // Description
+    boost::none,                                  // ObjectType
+    0,                                            // ObjectPlacement
+    0,                                            // Representation
+    boost::none,                                  // Tag
+		IfcSchema::IfcWallTypeEnum::IfcWallType_STANDARD
+  );
   // By adding a wall, a hierarchy has been automatically created that consists of the following
   // structure: IfcProject > IfcSite > IfcBuilding > IfcBuildingStorey > IfcWall
+  file.addBuildingProduct(spwand);
 
-  // An IfcOwnerHistory has been initialized as well, which should be assigned to the wall.
-  wall->setOwnerHistory(file.getSingle<IfcSchema::IfcOwnerHistory>());
-
-  // Since the spwand_shape consists only of planar faces and straight edges it can be serialized as an
+  // Since the spwand_shapeOCC consists only of planar faces and straight edges it can be serialized as an
   // IfcFacetedBRep. If it would not be a polyhedron, serialise() can only be successful when linked
   // to the IFC4 model and with `advanced` set to `true` which introduces IfcAdvancedFace. It would
   // return `0` otherwise.
-  IfcSchema::IfcProductDefinitionShape* object_shape = IfcGeom::serialise(spwand_shape, false);
-  file.addEntity(object_shape);
-  IfcSchema::IfcRepresentation* rep = *object_shape->Representations()->begin();
+  IfcSchema::IfcProductDefinitionShape* product_shape = IfcGeom::serialise(spwand_shapeOCC, false);
+  file.addEntity(product_shape);
+  IfcSchema::IfcRepresentation* rep = *product_shape->Representations()->begin();
   rep->setContextOfItems(file.getRepresentationContext("model"));
-  wall->setRepresentation(object_shape);
+  spwand->setRepresentation(product_shape);
   // A red colour is assigned to the wall.
-  file.setSurfaceColour(object_shape, 0.57 , 0.24 , 0.15);
+  file.setSurfaceColour(product_shape, 0.57 , 0.24 , 0.15);
     
   // Obtain a reference to the placement of the IfcBuildingStorey in order to create a hierarchy of placements for the products
   IfcSchema::IfcObjectPlacement* storey_placement = file.getSingle<IfcSchema::IfcBuildingStorey>()->ObjectPlacement();
 
-  // wall and is placed at the origin of the coordinate system.
-  wall->setObjectPlacement(file.addLocalPlacement(storey_placement));
+  // wall is placed at the origin of the coordinate system.
+  spwand->setObjectPlacement(file.addLocalPlacement(storey_placement));
 
 	// write the ifc File
   std::ofstream f("Spundwand.ifc");
